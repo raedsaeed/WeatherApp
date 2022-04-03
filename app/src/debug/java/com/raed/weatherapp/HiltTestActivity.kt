943e05f0b254d523795302dce3d86f6984fbcee1
Raed Saeed <raedsaeed46@hotmail.com>
2022-04-03 03:13:39 +0200

    Fix crashes and add test dependencies.

---
+++ app/src/debug/java/com/raed/weatherapp/HiltTestActivity.kt
@@ -0,0 +1,11 @@
+package com.raed.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+
+/**
+ * Created By Raed Saeed on 03/04/2022
+ */
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
